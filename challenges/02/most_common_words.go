60b12f4b73065099d718e0421107d95eacc05a3e
Kalina <kalinabuhleva@yahoo.com>
2015-08-09 11:54:40 +0300

    added solution

---
+++ challenges/02/most_common_words.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"sort"
+	"strings"
+)
+
+func getMostCommonWords(text string, wordCountLimit int) []string {
+
+	result := make([]string, 0)
+	result = append(result, getResult(strings.Split(strings.ToLower(text), " "), wordCountLimit)...)
+	sort.Strings(result)
+	return result
+}
+
+func getResult(text []string, count int) []string {
+	result := make([]string, 0)
+	mapCount := getCount(text)
+	for  key, value := range mapCount {
+		if value >= count {
+			result = append(result, key)
+		}
+	}
+	return result
+}
+
+func getCount(text []string) map[string]int {
+	mapCount := make(map[string]int)
+	for _, value := range text {
+		if _, ok := mapCount[value]; ok {
+			mapCount[value]++
+		} else {
+			mapCount[value] = 1
+		}
+	}
+	return mapCount
+}
